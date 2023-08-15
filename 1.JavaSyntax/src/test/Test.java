b6d212feec0bc5245cb95394b5fef2e22a68ca5b
CheFF77 <belikto.sandanov@gmail.com>
2023-08-15 19:58:33 +0300

    add solution javasyntax task03

---
+++ 1.JavaSyntax/src/test/Test.java
@@ -0,0 +1,9 @@
+package test;
+
+public class Test {
+    public static void main(String[] args) {
+        int a =5;
+        String name = "" + a + a + 1 + a;
+        System.out.println(name);
+    }
+}
