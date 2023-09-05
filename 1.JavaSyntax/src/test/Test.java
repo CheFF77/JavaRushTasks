735b931fb30b5548e1ba5762ea4e076f1723a2dd
CheFF77 <belikto.sandanov@gmail.com>
2023-09-05 22:37:09 +0300

    add solution javasyntax task05 continue

--- 1.JavaSyntax/src/test/Test.java
+++ 1.JavaSyntax/src/test/Test.java
@@ -4,9 +4,17 @@ import java.util.Scanner;
 
 public class Test {
     public static void main(String[] args) {
-        for(int i = 0; i < 10;)  {
-            System.out.println(i);
-
+        Scanner scanner = new Scanner(System.in);
+        int[] array = new int[10];
+        for (int i = 0; i < array.length; i++) {
+            array[i] = scanner.nextInt();
         }
+        int min = array [0];
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] < min) {
+                min = array[i];
+            }
+        }
+        System.out.println(min);
     }
 }
